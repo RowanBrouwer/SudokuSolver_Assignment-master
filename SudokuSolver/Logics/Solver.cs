f66ef1f9ccdcd08b0590e5835ff5d136be6a83fd
chris vergeer <C.H.M.Vergeer@student.tudelft.nl>
2020-06-23 12:53:53 +0200

    1st version guesser

--- SudokuSolver/Logics/Solver.cs
+++ SudokuSolver/Logics/Solver.cs
@@ -11,11 +11,6 @@ namespace SudokuSolver.Logics
 {
     public class Solver
     {
-
-        Boolean testx = false;
-        Boolean testy = false;
-        Boolean testcube = false;
-        Boolean trowback = false;
         int counter = 0;
 
         public int[][] Solve(int[][] sudoku)
@@ -28,33 +23,21 @@ namespace SudokuSolver.Logics
                     {
                         if (sudoku[a][b] == 0)
                         {
-                            counter = 0;
-                            var numberlist = Enumerable.Range(0, 10).ToList();
-                            for (int c = 0; c < 9; c++)
-                            {
-                                if (numberlist.Contains(sudoku[a][c]))
-                                {
-                                    numberlist.Remove(sudoku[a][c]);
-                                }
-                            }
-                            for (int d = 0; d < 9; d++)
-                            {
-                                if (numberlist.Contains(sudoku[d][b]))
-                                {
-                                    numberlist.Remove(sudoku[d][b]);
-                                }
-                            }
 
-                            numberlist = test(sudoku, a, b, numberlist);
+                            var numberlist = Enumerable.Range(0, 10).ToList();
+
+                            numberlist = horizontalchecker(sudoku, a, b, numberlist);
+
+                            numberlist = verticalchecker(sudoku, a, b, numberlist);
+
+                            numberlist = boxchecker(sudoku, a, b, numberlist);
 
                             if (numberlist.Count == 1)
                             {
                                 sudoku[a][b] = numberlist[0];
+                                counter = 0;
                             }
 
-
-
-                           
                         }
                         else
                         {
@@ -66,7 +49,88 @@ namespace SudokuSolver.Logics
             return sudoku;
         }
 
-        public List<int> test(int[][] sudoku, int a, int b, List<int> numberlist)
+
+        public int[][] SolveGuessing(int[][] sudoku)
+        {
+            int[][] startsudoku = new int[9][];
+            for (int i = 0; i < 9; i++)
+            {
+                startsudoku[i] = new int[9];
+                for (int j = 0; j < 9; j++)
+                {
+                    startsudoku[i][j] = sudoku[i][j];
+                }
+            }
+
+
+            while (counter < 700)
+            {
+                for (int a = 0; a < 9; a++)
+                {
+                    for (int b = 0; b < 9; b++)
+                    {
+                        if (sudoku[a][b] == 0)
+                        {
+
+                            var numberlist = Enumerable.Range(0, 10).ToList();
+
+                            numberlist = horizontalchecker(sudoku, a, b, numberlist);
+
+                            numberlist = verticalchecker(sudoku, a, b, numberlist);
+
+                            numberlist = boxchecker(sudoku, a, b, numberlist);
+
+
+                            Boolean solved = false;
+                            int numberlistID = 0;
+
+                            while (solved == false)
+                            {
+                                if (numberlist.Count <= 6 && numberlist.Count >0)
+                                {
+                                    sudoku[a][b] = numberlist[numberlistID];
+                                    sudoku = Solve(sudoku);
+                                    for (int i = 0; i < 9; i++)
+                                    {
+                                        for (int j = 0; j < 9; j++)
+                                        {
+                                            if (sudoku[i][j] == 0)
+                                            {
+                                                sudoku = startsudoku;
+                                                numberlistID++;
+
+                                            }
+                                            else
+                                            {
+                                                solved = true;
+                                            }
+                                        }
+                                    }
+                                    counter = 0;
+                                }
+                                else
+                                {
+                                    solved = true;
+                                }
+                            }
+                            
+                        }
+                    }
+                }
+                counter++;
+            }
+        return sudoku;
+        }
+
+        public int[][] Create(int[][] sudoku)
+        {
+            return sudoku;
+        }
+
+
+
+
+        public List<int> boxchecker(int[][] sudoku, int a, int b, List<int> numberlist)
         {
             if (a >= 0 && a <= 2)
             {
@@ -198,204 +262,30 @@ namespace SudokuSolver.Logics
             return numberlist;
         }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        public int[][] SolveGuessing(int[][] sudoku)
+        public List<int> horizontalchecker(int[][] sudoku, int a, int b, List<int> numberlist)
         {
-            return sudoku;
+            for (int c = 0; c < 9; c++)
+            {
+                if (numberlist.Contains(sudoku[a][c]))
+                {
+                    numberlist.Remove(sudoku[a][c]);
+                }
+            }
+            return numberlist;
         }
 
-        public int[][] Create(int[][] sudoku)
+        public List<int> verticalchecker(int[][] sudoku, int a, int b, List<int> numberlist)
         {
-            return sudoku;
-        }
-
-        public void checkerx(int[][] sudoku, int a, int sulotion)
-        {
-           
-            if (sudoku[a][0] != sulotion && sudoku[a][1] != sulotion && sudoku[a][2] != sulotion && sudoku[a][3] != sulotion && sudoku[a][4] != sulotion && sudoku[a][5] != sulotion && sudoku[a][6] != sulotion && sudoku[a][7] != sulotion && sudoku[a][8] != sulotion)
+            for (int d = 0; d < 9; d++)
             {
-                testx = true;
-            }
-            
-
-        }
-
-        public void checkery(int[][] sudoku, int b, int sulotion)
-        {
-            if (sudoku[0][b] != sulotion && sudoku[1][b] != sulotion && sudoku[2][b] != sulotion && sudoku[3][b] != sulotion && sudoku[4][b] != sulotion && sudoku[5][b] != sulotion && sudoku[6][b] != sulotion && sudoku[7][b] != sulotion && sudoku[8][b] != sulotion)
-            {
-                testy = true;
-            }
-        }
-
-        public void checkercube(int[][] sudoku, int a, int b, int sulotion)
-        {
-            if(a>=0 && a <= 2)
-            {
-                if (b >= 0 && b <= 2)
+                if (numberlist.Contains(sudoku[d][b]))
                 {
-                    if (sudoku[0][0] != sulotion && sudoku[0][1] != sulotion && sudoku[0][2] != sulotion && sudoku[1][0] != sulotion && sudoku[1][1] != sulotion && sudoku[1][2] != sulotion && sudoku[2][0] != sulotion && sudoku[2][1] != sulotion && sudoku[2][2] != sulotion)
-                    {
-                        testcube = true;
-                        return;
-                    }
-                }
-                else if (b >= 3 && b <= 5)
-                {
-                    if (sudoku[0][3] != sulotion && sudoku[0][4] != sulotion && sudoku[0][5] != sulotion && sudoku[1][3] != sulotion && sudoku[1][4] != sulotion && sudoku[1][5] != sulotion && sudoku[2][3] != sulotion && sudoku[2][4] != sulotion && sudoku[2][5] != sulotion)
-                    {
-                        testcube = true;
-                        return;
-                    }
-                }
-                else
-                {
-                    if (sudoku[0][6] != sulotion && sudoku[0][7] != sulotion && sudoku[0][8] != sulotion && sudoku[1][6] != sulotion && sudoku[1][7] != sulotion && sudoku[1][8] != sulotion && sudoku[2][6] != sulotion && sudoku[2][7] != sulotion && sudoku[2][8] != sulotion)
-                    {
-                        testcube = true;
-                        return;
-                    }
+                    numberlist.Remove(sudoku[d][b]);
                 }
             }
-            else if(a>=3 && a <= 5)
-            {
-                if (b >= 0 && b <= 2)
-                {
-                    if (sudoku[3][0] != sulotion && sudoku[3][1] != sulotion && sudoku[3][2] != sulotion && sudoku[4][0] != sulotion && sudoku[4][1] != sulotion && sudoku[4][2] != sulotion && sudoku[5][0] != sulotion && sudoku[5][1] != sulotion && sudoku[5][2] != sulotion)
-                    {
-                        testcube = true;
-                        return;
-                    }
-                }
-                else if (b >= 3 && b <= 5)
-                {
-                    if (sudoku[3][3] != sulotion && sudoku[3][4] != sulotion && sudoku[3][5] != sulotion && sudoku[4][3] != sulotion && sudoku[4][4] != sulotion && sudoku[4][5] != sulotion && sudoku[5][3] != sulotion && sudoku[5][4] != sulotion && sudoku[5][5] != sulotion)
-                    {
-                        testcube = true;
-                        return;
-                    }
-                }
-                else
-                {
-                    if (sudoku[3][6] != sulotion && sudoku[3][7] != sulotion && sudoku[3][8] != sulotion && sudoku[4][6] != sulotion && sudoku[4][7] != sulotion && sudoku[4][8] != sulotion && sudoku[5][6] != sulotion && sudoku[5][7] != sulotion && sudoku[5][8] != sulotion)
-                    {
-                        testcube = true;
-                        return;
-                    }
-                }
-
-            }
-            else
-            {
-                if (b >= 0 && b <= 2)
-                {
-                    if (sudoku[6][0] != sulotion && sudoku[6][1] != sulotion && sudoku[6][2] != sulotion && sudoku[7][0] != sulotion && sudoku[7][1] != sulotion && sudoku[7][2] != sulotion && sudoku[8][0] != sulotion && sudoku[8][1] != sulotion && sudoku[8][2] != sulotion)
-                    {
-                        testcube = true;
-                        return;
-                    }
-                }
-                else if (b >= 3 && b <= 5)
-                {
-                    if (sudoku[6][3] != sulotion && sudoku[6][4] != sulotion && sudoku[6][5] != sulotion && sudoku[7][3] != sulotion && sudoku[7][4] != sulotion && sudoku[7][5] != sulotion && sudoku[8][3] != sulotion && sudoku[8][4] != sulotion && sudoku[8][5] != sulotion)
-                    {
-                        testcube = true;
-                        return;
-                    }
-                }
-                else
-                {
-                    if (sudoku[6][6] != sulotion && sudoku[6][7] != sulotion && sudoku[6][8] != sulotion && sudoku[7][6] != sulotion && sudoku[7][7] != sulotion && sudoku[7][8] != sulotion && sudoku[8][6] != sulotion && sudoku[8][7] != sulotion && sudoku[8][8] != sulotion)
-                    {
-                        testcube = true;
-                        return;
-                    }
-                }
-            }
+            return numberlist;
         }
 
     }
 }
 
-
-
-
-
-
-
-
-//for( int a = 0; a < 9;a++ )
-//{
-//    for(int b = 0; b < 9; b++)
-//    {
-//        if(sudoku[a][b] == 0 || trowback == true)
-//        {
-//            int solution = 0;
-//            if (trowback == true)
-//            {
-//                solution = sudoku[a][b];
-//            }
-//            while (testx == false || testy == false || testcube ==false )
-//            {
-//                testx = false;
-//                testy = false;
-//                testcube = false;
-//                solution++;
-//                checkerx(sudoku,a,solution);
-//                checkery(sudoku, b, solution);
-//                checkercube(sudoku,a, b, solution);
-//                //breakmark
-//                if (solution > 9)
-//                {
-//                    testx = true;
-//                    testy = true;
-//                    testcube = true;
-//                    b = b - 2;
-//                    //if (b < 0)
-//                    //{
-//                    //    // b = 8;
-//                    //    // a = a - 2;
-//                    //    solution = 99;
-//                    //    b = 0;
-//                    //}
-//                    //else
-//                    //{
-//                        solution = 0;
-//                    //}
-//                    trowback = true;
-//                }
-//            }
-//            testx = false;
-//            testy = false;
-//            testcube = false;
-//            if(solution != 0)
-//            {
-//                trowback = false;
-//            }
-
-//            sudoku[a][b] = solution;
-//        }
-//        counter++;
-//        if (counter > 600)
-//        {
-//            return sudoku;
-//        }
-//    }
-//    counter = 0;
-//}
-
-
-//sudoku[1] = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
